f2df16e39ee263921545f30d45cee6c1a4bb682d
Simon Zeiger <monzeiger@gmail.com>
2021-01-28 19:32:17 -0800

    Added search bar in trip view activity.

--- Pathways/app/src/main/java/com/example/pathways/TripViewActivity.java
+++ Pathways/app/src/main/java/com/example/pathways/TripViewActivity.java
@@ -6,12 +6,14 @@ import android.widget.TextView;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 import androidx.fragment.app.FragmentActivity;
 
 public class TripViewActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap _map;
-
+    private PlacesClient _placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,12 @@ public class TripViewActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyDXx6nHhNO_jNJiFm0ZMp7KPOSK6USBBEg");
+        }
+
+        _placesClient = Places.createClient(this);
 
     }
 
