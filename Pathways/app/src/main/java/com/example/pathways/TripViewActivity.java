1fdb1ff8c12ecded880367d76176e1029fbe15bc
simonzeiger <monzeiger@gmail.com>
2021-01-29 16:16:59 -0800

    Add search bar to Trip View Activity (#32)
    * Added search bar in trip view activity.
    
    * Added search bar in trip view activity.
    
    This utilizes the Google placse API.

--- Pathways/app/src/main/java/com/example/pathways/TripViewActivity.java
+++ Pathways/app/src/main/java/com/example/pathways/TripViewActivity.java
@@ -1,17 +1,29 @@
 package com.example.pathways;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
 
 import androidx.fragment.app.FragmentActivity;
 
+import java.util.Arrays;
+
 public class TripViewActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap _map;
-
+    private PlacesClient _placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,38 @@ public class TripViewActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyDXx6nHhNO_jNJiFm0ZMp7KPOSK6USBBEg");
+        }
+
+        _placesClient = Places.createClient(this);
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_frag);
+        autocompleteFragment.setHint("Add stop");
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,
+                Place.Field.LAT_LNG, Place.Field.ADDRESS, Place.Field.RATING));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                _map.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 4f));
+                _map.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName())
+                        .icon(BitmapDescriptorFactory
+                                .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("TAG", "An error occurred: " + status);
+            }
+        });
 
     }
 
