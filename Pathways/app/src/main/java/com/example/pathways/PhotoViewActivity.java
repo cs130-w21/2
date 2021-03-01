7d026d157fe028c3b99830fc9e93807fcdc18c73
Simon Zeiger <monzeiger@gmail.com>
2021-03-01 14:02:12 -0800

    Removed title bar and made background black in photoview

--- Pathways/app/src/main/java/com/example/pathways/PhotoViewActivity.java
+++ Pathways/app/src/main/java/com/example/pathways/PhotoViewActivity.java
@@ -5,21 +5,27 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.OnOutsidePhotoTapListener;
 import com.github.chrisbanes.photoview.PhotoView;
 
 import java.io.FileNotFoundException;
 
-public class PhotoViewActivity extends AppCompatActivity {
+public class PhotoViewActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_view);
+        getSupportActionBar().hide();
 
         Uri targetUri = Uri.parse(getIntent().getStringExtra("URI"));
         Bitmap bitmap;
 
         PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setOnOutsidePhotoTapListener(imageView -> PhotoViewActivity.this.finish());
 
         try {
             bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
@@ -29,4 +35,5 @@ public class PhotoViewActivity extends AppCompatActivity {
         }
 
     }
+
 }
